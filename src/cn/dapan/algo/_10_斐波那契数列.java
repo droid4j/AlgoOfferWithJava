e93238eadcbb919089cbc5e6c714158c60110679
zhangpanzhao <zhangpanzhao@okay.cn>
2020-05-01 22:02:02 +0800

    [update] 非递归实现第10号题

--- src/cn/dapan/algo/_10_斐波那契数列.java
+++ src/cn/dapan/algo/_10_斐波那契数列.java
@@ -27,11 +27,35 @@ public class _10_斐波那契数列 {
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 
+    /**
+     * 递归虽然简洁，但是有两个非常明显的缺点：
+     *  1. 重复计算，比如：n = 5时，f(5) = f(4) + f(3), f(4) = f(3) + f(2)，从这两个式子中，可以看出 f(3)被计算了两次
+     *  2. 当调用栈比较深时，程序会变得非常的慢，甚至报错
+     *
+     *  非递归方式
+     *  1. 只需要一层for循环即可，避免了方法的重复调用
+     *  2. 每一轮的结果保存到了 ret，结果不需要重复计算了
+     *
+     */
+    public int Fibonacci2(int n) {
+        if (n < 2) return n;
+
+        int f1 = 0;
+        int f2 = 1;
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = f2 + f1;
+            f1 = f2;
+            f2 = ret;
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         _10_斐波那契数列 solution = new _10_斐波那契数列();
-        System.out.println("n= 0, result=" + solution.Fibonacci(0));
-        System.out.println("n= 1, result=" + solution.Fibonacci(1));
-        System.out.println("n= 5, result=" + solution.Fibonacci(5));
+        System.out.println("n= 0, result=" + solution.Fibonacci2(0));
+        System.out.println("n= 1, result=" + solution.Fibonacci2(1));
+        System.out.println("n= 5, result=" + solution.Fibonacci2(5));
     }
 
 }
